13d39c9d47178593ff903aaede3383d0aaf66eb6
nightmare226 <miltonjohnston226@gmail.com>
2024-10-28 04:19:26 -0400

    fix: delete github avatar

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,11 +9,11 @@ function Header({ children }) {
                 <strong>Options</strong>
             </h1>
             {children}
-            <div className='actions'>
+            {/* <div className='actions'>
                 <a target='_blank' href='https://github.com/miltonjohnston/car-configurator' title='GitHub'>
                     <GitHubIcon className='icon' />
                 </a>
-            </div>
+            </div> */}
         </div>
     )
 }
